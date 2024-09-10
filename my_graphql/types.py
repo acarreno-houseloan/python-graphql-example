66aa4e888e309e61ad1aad541568d34c46b36f60
Adrian Carreno <adrian.carreno@gomoder.com>
2024-09-10 18:52:11 -0300

    refactor(graphql.types): define direct properties

--- my_graphql/types.py
+++ my_graphql/types.py
@@ -1,5 +1,4 @@
 import graphene
-from graphene import relay
 from graphene_mongo import MongoengineObjectType
 
 from database.models import Comment as CommentModel
@@ -11,27 +10,25 @@ from database.models import User as UserModel
 class User(MongoengineObjectType):
     class Meta:
         model = UserModel
-        interfaces = (relay.Node,)
-
-    username = graphene.String()
 
 
 class Post(MongoengineObjectType):
     class Meta:
         model = PostModel
-        interfaces = (relay.Node,)
-        exclude_fields = ("author_id",)
+
+    author = graphene.Field(User)
 
 
 class Comment(MongoengineObjectType):
     class Meta:
         model = CommentModel
-        interfaces = (relay.Node,)
-        exclude_fields = ("author_id", "post_id")
+
+    author = graphene.Field(User)
+    post = graphene.Field(Post)
 
 
 class Like(MongoengineObjectType):
     class Meta:
         model = LikeModel
-        interfaces = (relay.Node,)
-        exclude_fields = ("user_id", "post_id")
+
+    user = graphene.Field(User)
