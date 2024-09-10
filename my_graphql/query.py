2593597423bd667e685d9713ce5f6be699de0033
Adrian Carreno <adrian.carreno@gomoder.com>
2024-09-10 20:52:52 -0300

    doc(readme): add information on how to query

--- my_graphql/query.py
+++ my_graphql/query.py
@@ -14,19 +14,41 @@ from .types import User as UserType
 
 class Query(graphene.ObjectType):
     node = Node.Field()
-    users = graphene.List(UserType)
-    posts = graphene.List(PostType)
-    comments = graphene.List(CommentType)
-    likes = graphene.List(LikeType)
+    users = graphene.List(
+        UserType,
+        id=graphene.Int(),
+        username=graphene.String(),
+        email=graphene.String(),
+        created_at=graphene.String(),
+    )
+    posts = graphene.List(PostType, id=graphene.Int())
+    comments = graphene.List(CommentType, id=graphene.Int())
+    likes = graphene.List(LikeType, id=graphene.Int())
 
-    def resolve_users(self, _info):
-        return list(UserModel.objects.all())  # type: ignore
+    def resolve_users(self, _info, **kwargs):
+        query = {}
+        if "id" in kwargs:
+            query["id"] = kwargs.pop("id")
+        if "username" in kwargs:
+            query["username"] = kwargs.pop("username")
+        if "email" in kwargs:
+            query["email"] = kwargs.pop("email")
+        return list(UserModel.objects(**query).all())  # type: ignore
 
-    def resolve_posts(self, _info):
-        return list(PostModel.objects.all())  # type: ignore
+    def resolve_posts(self, _info, **kwargs):
+        query = {}
+        if "id" in kwargs:
+            query["id"] = kwargs.pop("id")
+        return list(PostModel.objects(**kwargs).all())  # type: ignore
 
-    def resolve_comments(self, _info):
-        return list(CommentModel.objects.all())  # type: ignore
+    def resolve_comments(self, _info, **kwargs):
+        query = {}
+        if "id" in kwargs:
+            query["id"] = kwargs.pop("id")
+        return list(CommentModel.objects(**kwargs).all())  # type: ignore
 
-    def resolve_likes(self, _info):
-        return list(LikeModel.objects.all())  # type: ignore
+    def resolve_likes(self, _info, **kwargs):
+        query = {}
+        if "id" in kwargs:
+            query["id"] = kwargs.pop("id")
+        return list(LikeModel.objects(**kwargs).all())  # type: ignore
