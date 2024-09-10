dd9c01d8ef81bc2b180c78757db075121decb01e
Adrian Carreno <adrian.carreno@gomoder.com>
2024-09-10 18:49:36 -0300

    feat(server): add uvicorn server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 
+import uvicorn
 from mongoengine import connect
 from starlette.applications import Starlette
 from starlette_graphene3 import GraphQLApp
@@ -13,3 +14,6 @@ db = client[os.environ["DATABASE_NAME"]]
 app = Starlette()
 
 app.mount("/", GraphQLApp(schema, on_get=make_graphiql_handler()))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
